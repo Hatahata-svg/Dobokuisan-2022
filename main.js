56cb6a81611fe14f04cbc245e1a8817c955afe4b
Hatahata-svg <71586061+Hatahata-svg@users.noreply.github.com>
2023-10-01 22:52:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -16,27 +16,6 @@
 //}).setText('渋谷ヒカリエ');
 //var marker = new maplibregl.Marker().setLngLat([139.70356, 35.65901]).setPopup(popup).addTo(map);
 
-// ポイントデータを表示する
-//map.on('load', function () {
-//    map.loadImage("C:\Users\sokag\Desktop\プログラミング\プログラミング\Python\GIS\MapLibre GL JS\つり橋アイコン1.png", function (error, image) {
-//        if (error) throw error;
-//        map.addImage('facility_icon', image);
-//    });
-//    map.addSource('facility_point', {
-//        type: 'geojson',
-//        data: "./csvtogeojson/土木学会選奨土木遺産2022.geojson",
-//    });
-//    map.addLayer({
-//        id: 'facility_point',
-//        type: 'symbol',
-//        source: 'facility_point',
-//        layout: {
-//            'icon-image': 'facility_icon',
-//            'icon-size': 1,
-//        },
-//    });
-//})
-
 map.on('load', function () {
     map.loadImage('./JPG/つり橋アイコン1.png', function (error, image) {
         if (error) throw error;
